1e40e8e091798840729d55e88f14ecd715adecd5
tanzilmia <ziltan590@gmail.com>
2022-10-25 19:00:29 +0600

    courseDetails

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const courese = require('./Data/courseDetails.json')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/course',(req,res)=>{
+  res.send(courese)
 })
 
 app.listen(port, () => {
